e150c93c678c3b1f3fe2c647a54204ecd5ce4780
busracandan <busra58cndn@hotmail.com>
2018-02-24 15:51:44 +0200

    dördüncü projem

---
+++ txtden grıdviewe/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace txtden_grıdviewe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr=new StreamReader("deneme.txt",Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+                string[] ayır = s.Split(',');
+                dataGridView1.Rows.Add(ayır);
+            }
+            sr.Close();
+        }
+    }
+}
